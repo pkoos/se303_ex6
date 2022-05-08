c79881a0b60d72b60fdb2cdcd51fa2aa3aae5286
Paul Koos <paully_k5298@yahoo.com>
2022-05-07 20:13:38 -0700

    going crazy with objects! Create a parent/child relationship of Color, DarkModeColor, and LigthModeColor. Give option of base classes to use whatever value they would like, but DarkModeDisplay and LightModeDisplay use specific Colors. Also create coordinate class to hold the x and y values.

--- exercise_six.rb
+++ exercise_six.rb
@@ -4,43 +4,83 @@ class ButtonDisplay
 	end
 
 	def draw_button
-		paint(button_data.label_text, button_data.x, button_data.y, button_data.foreground_color - 10, button_data.color_string)
+		paint(button_data.label_text, 
+			button_data.coordinates.x, 
+			button_data.coordinatesy, 
+			button_data.color.fore_color, 
+			button_data.color.color_string)
 	end
 end
 
-class ButtonData
-	attr_reader :label :x :y :fore_color :color_string
-	def initialize(label, x, y, fore_color, color_string)
-		@label = label
-		@x = x
-		@y = y
+class DarkModeColor < Color
+	def initialize(color_string = '#111111')
+		super
+	end
+	def color_string
+		'#111111'
+	end
+	def fore_color
+		return @fore_color - 10
+	end
+end
+
+class LightModeColor < Color
+	def initialize()
+		super
+	end
+	def color_string
+		'#E0E0E0'
+	end
+	def fore_color
+		return @fore_color + 10
+	end
+
+class Color
+	attr_reader :color_string :fore_color
+	def initialize(fore_color, color_string)
 		@fore_color = fore_color
 		@color_string = color_string
 	end
 end
 
+class Coordinate
+	attr_reader :x :y
+	def initialize(x, y)
+	end
+end
+
+class ButtonData
+	attr_reader :label :x :y :color :coordinates
+	def initialize(label, coordinates, color)
+		@label = label
+		@coordinates = coordinates
+		@color = color
+	end
+end
+
 class DarkModeButton < ButtonData
-	def initialize(color_string = '#111111')
+	def initialize
 		super
-		@fore_color = fore_color -10
-		@color_string = color_string
+		@color = DarkModeColor.new
 	end
 end
 
 class LightModeButton < ButtonData
-	def initialize(color_string = 'E0E0E0')
+	def initialize
 		super
-		@fore_color = fore_color + 10
-		@color_string color_string
+		@color = LightModeColor.new
+	end
 end
 
 def set_button_display(dark_mode)
 	if dark_mode
-		ButtonDisplay.new(DarkModeButton.new(label, x, y, fore_color).draw_button
+		ButtonDisplay.new(DarkModeButton.new(label, coordinates).draw_button
 	else
-		ButtonDisplay.new(LightModeButton.new(label, x, y, fore_color).draw_button
+		ButtonDisplay.new(LightModeButton.new(label, coordinates).draw_button
 	end
 end
+
+
 
 button_dark = set_button_display(true)
 button_light = set_button_disp(false)
