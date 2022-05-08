0bc66a079d46dd503e5a6f86e16bb30012249e3d
Paul Koos <paully_k5298@yahoo.com>
2022-05-07 19:36:48 -0700

    refactor and remove all parameters from calls to draw_button.

--- exercise_six.rb
+++ exercise_six.rb
@@ -1,21 +1,3 @@
-def draw_button(is_dark_mode, button_data)
-  if is_dark_mode
-    # darken foreground color for dark mode
-    draw_dark_button(button_data)
-  else
-    # lighten foreground color for non-dark mode
-    draw_light_button(button_data)
-  end
-end
-
-def draw_dark_button(button_data)
-	paint(button_data.label_text, button_data.x, button_data.y, button_data.foreground_color - 10, '#111111')
-end
-
-def draw_light_button(button_data)
-	paint(button_data.label_text, button_data.x, button_data.y, button_data.foreground_color + 10, '#E0E0E0')
-end
-
 class ButtonDisplay
 	def initialize(button_data, is_dark_mode)
 		...
@@ -29,6 +11,14 @@ class ButtonDisplay
 			# lighten foreground color for non-dark mode
 			draw_light_button(button_data)
 		end
+	end
+
+	def draw_dark_button(button_data)
+		paint(button_data.label_text, button_data.x, button_data.y, button_data.foreground_color - 10, '#111111')
+	end
+	
+	def draw_light_button(button_data)
+		paint(button_data.label_text, button_data.x, button_data.y, button_data.foreground_color + 10, '#E0E0E0')
 	end
 end
 
@@ -49,9 +39,9 @@ end
 
 
 button_data = ButtonData.new(label, x, y, fore_color, color_string_here)
-
+button_display = ButtonDisplay.new(button_data, is_dark_mode)
 if dark_mode_flag
-	draw_button(label_data, x, y, foreground_color, dark_mode_flag, button_data)
+	draw_button(dark_mode_flag, button_data)
 else
-	draw_button(label_data, x, y, foreground_color, dark_mode_flag, button_data)
+	draw_button(dark_mode_flag, button_data)
 end
