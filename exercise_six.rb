b79d6744d472611bc5a80f1e766fa9784ee150f9
Paul Koos <paully_k5298@yahoo.com>
2022-05-07 19:34:47 -0700

    combine the draw functions into their own class.

--- exercise_six.rb
+++ exercise_six.rb
@@ -16,6 +16,21 @@ def draw_light_button(button_data)
 	paint(button_data.label_text, button_data.x, button_data.y, button_data.foreground_color + 10, '#E0E0E0')
 end
 
+class ButtonDisplay
+	def initialize(button_data, is_dark_mode)
+		...
+	end
+
+	def draw_button(is_dark_mode, button_data)
+		if is_dark_mode
+			# darken foreground color for dark mode
+			draw_dark_button(button_data)
+		else
+			# lighten foreground color for non-dark mode
+			draw_light_button(button_data)
+		end
+	end
+end
 
 class ButtonData
 	attr_reader :label :x :y :fore_color :color_string
